ddb72d45759c2ef9e82999e686e436d2ce656e09
German123ss <germanmendieta123ss@gmail.com>
2024-04-27 14:37:17 -0300

    allow cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	_ "github.com/lib/pq"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -13,11 +14,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	http.HandleFunc("POST /proveedores", insertProveedorHandler)
-	http.HandleFunc("POST /productos_proveedor", insertProductoProveedorHandler)
-	http.HandleFunc("GET /proveedores", proveedoresHandler)
-	http.HandleFunc("GET /productos_proveedor", productosProveedorHandler)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /proveedores", insertProveedorHandler)
+	mux.HandleFunc("POST /productos_proveedor", insertProductoProveedorHandler)
+	mux.HandleFunc("GET /proveedores", proveedoresHandler)
+	mux.HandleFunc("GET /productos_proveedor", productosProveedorHandler)
+
+	// Configura el middleware de CORS
+	handler := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"}, // orígenes que deseas permitir
+		AllowedMethods: []string{"GET", "POST"},
+		AllowedHeaders: []string{"Content-Type"},
+	}).Handler(mux)
 
 	log.Println("Servidor iniciado en el puerto 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
